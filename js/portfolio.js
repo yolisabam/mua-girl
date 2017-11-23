2e7adeaf1853c810908d57b754ae19b80961f1c5
Yolisa Bam <yolisabam@Yolisas-MacBook-Pro.local>
2017-11-22 17:15:45 -0800

    resizing images for work section

---
+++ js/portfolio.js
@@ -0,0 +1,50 @@
+window.__portfolio = [
+  {
+    image: "img/evie-1.jpg",
+    albumTiitle: "DAMN.",
+    releaseDate: "APRIL 14, 2017",
+    buyLink: "https://itunes.apple.com/us/album/damn/id1223592280",
+  },
+  {
+    image: "img/evie-2.jpg",
+    albumTiitle: "UNTITLED UNMASTERED",
+    releaseDate: "MARCH 4, 2016",
+    buyLink: "https://itunes.apple.com/us/album/untitled-unmastered/id1089846273",
+  },
+  {
+    image: "img/evie-3.jpg",
+    albumTiitle: "TO PIMP A BUTTERFLY",
+    releaseDate: "MARCH 15, 2015",
+    buyLink: "https://itunes.apple.com/ca/album/to-pimp-a-butterfly/id974187289",
+  },
+  {
+    image: "img/album-03-blk.jpg",
+    albumTiitle: "GOOD KID, M.A.A.D CITY",
+    releaseDate: "OCTOBER 22, 2012",
+    buyLink: "https://itunes.apple.com/us/album/good-kid-m-a-a-d-city-deluxe-version/id622710488",
+  },
+   {
+    image: "img/album-05-blk.jpg",
+    albumTiitle: "DAMN.",
+    releaseDate: "APRIL 14, 2017",
+    buyLink: "https://itunes.apple.com/us/album/damn/id1223592280",
+  },
+  {
+    image: "img/album-00-blk.jpg",
+    albumTiitle: "UNTITLED UNMASTERED",
+    releaseDate: "MARCH 4, 2016",
+    buyLink: "https://itunes.apple.com/us/album/untitled-unmastered/id1089846273",
+  },
+  {
+    image: "img/album-02-blk.jpg",
+    albumTiitle: "TO PIMP A BUTTERFLY",
+    releaseDate: "MARCH 15, 2015",
+    buyLink: "https://itunes.apple.com/ca/album/to-pimp-a-butterfly/id974187289",
+  },
+  {
+    image: "img/album-03-blk.jpg",
+    albumTiitle: "GOOD KID, M.A.A.D CITY",
+    releaseDate: "OCTOBER 22, 2012",
+    buyLink: "https://itunes.apple.com/us/album/good-kid-m-a-a-d-city-deluxe-version/id622710488",
+  },
+]
